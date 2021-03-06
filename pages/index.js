8a8ce5d1210f56b371482bc1b341843997458e85
sirchras <cjlynch@hotmail.co.nz>
2021-03-06 18:21:01 +1300

    add content to blog pages, styling

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import Head from 'next/head'
+import Link from 'next/link'
 
 import Layout, { siteTitle } from '../components/layout'
+import Date from '../components/date'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
 
@@ -35,11 +37,13 @@ export default function Home ({ allPostsData }) {
           {
             allPostsData.map(({ id, date, title }) => (
               <li className={utilStyles.listItem} key={id}>
-                {title}
+                <Link href={`/posts/${id}`}>
+                  <a>{title}</a>
+                </Link>
                 <br />
-                {id}
-                <br />
-                {date}
+                <small className={utilStyles.lightText}>
+                  <Date dateString={date}/>
+                </small>
               </li>
             ))
           }
